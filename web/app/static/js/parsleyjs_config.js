f23747507cabd460d17fbd89018f0dcf187e8a48
Adam Piskorski <adam@piskorski.org>
2016-02-09 04:17:33 +0200

    Fix parsley bootstrap integration

--- web/app/static/js/parsleyjs_config.js
+++ web/app/static/js/parsleyjs_config.js
@@ -1,20 +1,14 @@
 window.ParsleyConfig = {
-    classHandler: function (elem) {
-        // Return the $element that will receive these above success or error
-        // classes Could also be (and given directly from DOM) a valid selector like '#div'
-        return $(elem).closest(".form-group");
-    },
-
-
-    errorsContainer: function ( elem, isRadioOrCheckbox ) {
-        // Return the $element where errors will be appended Could also be
-        // (and given directly from DOM) a valid selector like '#div'
-        return $(elem).closest(".help-block")
-    },
-
 
     errorsWrapper: '<span class="help-block"></span>',
     errorTemplate: '<span></span>',
     successClass: 'has-success',           // Class name on each valid input
-    errorClass: 'has-error'               // Class name on each invalid input
+    errorClass: 'has-error',               // Class name on each invalid input
+
+    classHandler: function (el) {
+        // Return the $element that will receive these above success or error
+        // classes Could also be (and given directly from DOM) a valid selector like '#div'
+        return el.$element.closest(".form-group");
+    },
+
 };
