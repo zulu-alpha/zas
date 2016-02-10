731ee304d1c6c3a4cea5776d522967aad9244c2e
Adam Piskorski <adam@piskorski.org>
2016-02-10 19:11:07 +0200

    Move db path to config file

--- config/backup_config.py
+++ config/backup_config.py
@@ -3,6 +3,7 @@ CONFIG = {
     'user': 'username here',
     'passwd': 'password here',
     'log': '/var/log/backup/backup.log',
+    'path': '/data/db',
     'format': '%Y-%m-%d_%H-%M-%S',
-    'keep': 10
+    'keep': 10,
 }
