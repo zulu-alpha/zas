a28d1cea6d4b9816d939c96751d208d5e48364f7
Adam Piskorski <adam@piskorski.me>
2022-01-07 19:09:33 +0200

    feat: Add django-allauth

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path("admin/", admin.site.urls),
 ]
