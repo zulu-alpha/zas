d24b9e0d32e7588cd18ce67e0d8cc05c5a616759
Adam Piskorski <adam@piskorski.me>
2022-04-17 18:16:14 +0200

    feat: Get profile home working

--- config/settings.py
+++ config/settings.py
@@ -217,6 +217,6 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ),
 }
--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,18 @@
 from django.views.generic import TemplateView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 class ProfileView(TemplateView):
     template_name = "profiles/profile.j2"
 
 
-class HomeView(TemplateView):
+class HomeView(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    renderer_classes = [TemplateHTMLRenderer]
     template_name = "profiles/home.j2"
+
+    def get(self, request):
+        return Response()
